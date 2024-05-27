065ec0a2c5fc7c32ade1461cc1a5d39596bd859e
Tharun <tarun.venkat2013@gmail.com>
2024-05-27 18:57:56 +0530

    Added the headers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./component/Header";
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>This is header outside the div</h1>
+      <div>
+        {/* <Header></Header>  */}
+        {/* <h1>This is a header</h1> */}
+        This is my App
+      </div>
+    </React.Fragment>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
